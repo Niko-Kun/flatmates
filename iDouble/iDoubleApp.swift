1ea543313cffae5872bd0db3dbfd95d1aeafae17
Amabile <nikoamazon88@gmail.com>
2024-11-04 10:25:33 +0100

    Initial Commit

---
+++ iDouble/iDoubleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDoubleApp.swift
+//  iDouble
+//
+//  Created by Nicolò Amabile on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iDoubleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
