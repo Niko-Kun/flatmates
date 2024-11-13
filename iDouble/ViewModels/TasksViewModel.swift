0c440cf1c7ffa443554ba916deb18fa516faf5f6
Cristofor <cristofor.doarme@gmail.com>
2024-11-13 15:35:15 +0100

    Created CardViewTask and ListTaskView with components, added Colors in the Asset and Defined MainColor

---
+++ iDouble/ViewModels/TasksViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TasksViewModel.swift
+//  iDouble
+//
+//  Created by Cristofor Doamre on 13/11/24.
+//
+class TasksViewModel{
+    
+    var ListTasks: [Task] = [
+        Task(),
+        Task(),
+        Task(),
+        Task(),
+    ]
+}
---
+++ iDouble/Views/Task/Components/SmallCardGamificationView.swift
@@ -0,0 +1,18 @@
+//
+//  SmallCardGamificationView.swift
+//  iDouble
+//
+//  Created by Cristofor Doamre on 13/11/24.
+//
+
+import SwiftUI
+
+struct SmallCardGamificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallCardGamificationView()
+}
